192bb1e6e242b389f95a25524154b8f5e0421015
DimitriTedom <dimitritedom@gmail.com>
2024-10-17 16:24:18 +0100

    Footer Finished

--- src/components/Footers/SocialComponent.tsx
+++ src/components/Footers/SocialComponent.tsx
@@ -1,15 +1,25 @@
-import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineFacebook, AiOutlineTwitter, AiOutlineYoutube, AiOutlineInstagram } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
-import '../../index.css'
-interface SocialComponentProps{
-    path:string;
-    text:string;
-}
-const SocialComponent = ({path,text}:SocialComponentProps) => {
+const socialLinks = [
+  {name:'Facebook',icon: AiOutlineFacebook, url:'/'},
+  {name:'Twitter',icon: AiOutlineTwitter, url:'/'},
+  {name:'Youtube',icon: AiOutlineYoutube, url:'#'},
+  {name:'Instagram',icon: AiOutlineInstagram, url:'#'},
+];
+const SocialComponent = () => {
   return (
-    <div className='flex space-x-4 hover:text-black dark:text-white/40 dark:hover:text-white text-gray-400'>
-      <div><AiOutlineFacebook/>{path}</div>    
-      <h6 className="text-[1.8rem] font-normal">{text}</h6>
+    <div className='space-y-6'>
+      {socialLinks.map((social,index)=>{
+        const IconComponent = social.icon;
+
+        return(
+          <Link key={index} to={social.url} className='flex items-center space-x-2 text-[1.8rem] font-normal  hover:text-black dark:text-white/40 dark:hover:text-white text-gray-400'>
+          <IconComponent size={24}/>
+          <span>{social.name}</span>
+          </Link>
+        )
+      })}
     </div>
   )
 }
