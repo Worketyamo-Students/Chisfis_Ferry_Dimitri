2321af42e12a962246d13b8a70ca7067f5e02e26
ferry237 <youbiferry@gmail.com>
2024-10-29 10:58:02 +0100

    Revert "section Top 10 authors of the month"
    This reverts commit 29888d522b01a5548dcf3f4caf88e08cbbfb556a.

--- src/components/pages/Home/Home.Components.tsx
+++ src/components/pages/Home/Home.Components.tsx
@@ -1,7 +1,14 @@
 import HeaderComponent from '@/components/header.Component';
 import FooterComponent from '@/components/Footer.Component';
 import BenefitsComponent from '@/components/HomeComponents/Benefits.Component';
+<<<<<<< HEAD
 import CarouselParent from '@/components/HomeComponents/SuggestionsForDiscovery/CarouselParent';
+=======
+import Carousel1 from '@/components/HomeComponents/carousel1/carousel1';
+import CarouselParent
+  from '@/components/HomeComponents/SuggestionsForDiscovery/CarouselParent';
+
+>>>>>>> parent of 29888d5 (section Top 10 authors of the month)
 import JoinNewsLetter from './Join-News/JoinNewsLetter';
 function HomeComponent() {
 
@@ -10,9 +17,16 @@ function HomeComponent() {
       <section className='flex flex-col text-[3rem] gap-28'>
 
          <HeaderComponent/>
+<<<<<<< HEAD
         <BenefitsComponent/>
         <CarouselParent/>
         <JoinNewsLetter/>
+=======
+         <Carousel1/>
+         <BenefitsComponent/>
+         <CarouselParent/>
+         <JoinNewsLetter/>
+>>>>>>> parent of 29888d5 (section Top 10 authors of the month)
          <FooterComponent/>
 
       </section>
